c60804bc438d7b0fbe0400b0f6b214ff5d5cfbd0
pszabo94 <peter.szabo.994@gmail.com>
2025-09-04 11:42:45 +0200

    Update caddy.sh

--- containers/caddy.sh
+++ containers/caddy.sh
@@ -3,7 +3,7 @@
 podman create --replace \
   --name caddy \
   --publish 443:443/tcp \
-  --network slirp4netns \
+  --network host \
   -e TZ=Europe/Budapest \
   --user 0:0 \
   --volume /srv/ctnuser/containers/caddy/Caddyfile:/etc/caddy/Caddyfile:z \
